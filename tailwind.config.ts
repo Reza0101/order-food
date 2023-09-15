011c7af3d624a6a2eba08a33b6224561d5344edb
RezaSiahi <rezasiahi8@gmail.com>
2023-09-15 18:12:25 +0330

    add custom color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,45 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      'tint': {
+        1: '##5F2E9',
+        2: '#CAE4D3',
+        3: '#B0D7BD',
+        4: '#96C9A7',
+        5: '#7CBC91',
+        6: '#61AE7B',
+        7: '#4E9968'
+      },
+      'shade': {
+        1: '#396F4B',
+        2: '#315F41',
+        3: '#294F36',
+        4: '#21402B',
+        5: '#183020',
+        6: '#102016',
+        7: '#08100B'
+      },
+      'gray': {
+        1: '#F9F9F9',
+        2: '#E1E1E1',
+        3: '#EDEDED',
+        4: '#CBCBCB',
+        5: '#ADADAD',
+        6: '#757575',
+        7: '#717171',
+        8: '#353535',
+      },
+      'error': '#C30000',
+      'error-light': '#ED2E2E',
+      'success': '#00966D',
+      'success-light': '#00BA88',
+      'black': '#0C0C0C',
+      'primary': '#417F56',
+      'warning': '#A9791C',
+      'warning-light': '#F4B740',
+
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',

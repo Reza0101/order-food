5cb340b95a1e0d9b2430423518a1a691551368ed
RezaSiahi <rezasiahi8@gmail.com>
2023-09-20 19:00:16 +0330

    create slider food component

--- components/Box-food.tsx
+++ components/Box-food.tsx
@@ -1,9 +1,15 @@
 import { AiTwotoneHeart } from "react-icons/ai";
 import { AiOutlineHeart } from "react-icons/ai";
 
-const boxFood = ({ img, name, price, star, score, fodes, like }: any) => {
+const BoxFood = ({ img, name, price, star, score, fodes, like }: any) => {
   const discountedAmount = score && (score * price) / 100;
-  const starImageSrc = ["./Images/star/Rate1.png", "./Images/stare/Rate2.png", "./Images/star/Rate3.png", "./Images/star/Rate4.png", "./Images/star/Rate5.png"]
+  const starImageSrc = [
+    "./Images/star/Rate1.png",
+    "./Images/stare/Rate2.png",
+    "./Images/star/Rate3.png",
+    "./Images/star/Rate4.png",
+    "./Images/star/Rate5.png",
+  ];
 
   return (
     <div className="w-[168px] h-[231px] md:w-[288px] md:h-[433px] rounded-4 md:rounded-8 border border-gray-4">
@@ -12,37 +18,41 @@ const boxFood = ({ img, name, price, star, score, fodes, like }: any) => {
         className="h-[109px] md:h-[256px] w-full rounded-t-4 md:rounded-t-8"
         alt=""
       />
-      <p className="text-[12px] md:text-[20px]">{name}</p>
-      <div className="flex justify-between items-center">
-        <div className="flex items-center gap-1">
-          <p className="px-[4px] text-error bg-[#FFF2F2]">{score}%</p>
-          <p className="line-through text-gray-5">{price}</p>
-        </div>
+      <p className="text-[12px] text-center py-2 font-[400] md:text-[20px]">
+        {name}
+      </p>
+      <div className="flex flero px-[8px] md:px-[16px] justify-between items-center">
         {like ? (
-          <AiTwotoneHeart className="w-[16px] h-[16px] text-error" />
+          <AiTwotoneHeart className="w-[16px] cursor-pointer h-[16px] text-error" />
         ) : (
-          <div className="flex items-center text-gray-4">
-            <p className="md:hidden">افزودن به علاقه مندی ها</p>
-            <AiOutlineHeart className="w-[16px] h-[16px]" />
+          <div className="flex gap-1 items-center text-gray-4">
+            <AiOutlineHeart className="w-[16px] cursor-pointer h-[16px]" />
+            <p className="hidden md:block text-[10px]">افزودن به علاقه مندی ها</p>
           </div>
         )}
+        <div className="flex items-center gap-1 text-[10px]">
+          <p className="px-[4px] text-error bg-[#FFF2F2] rounded-8">{score}%</p>
+          <p className="line-through text-gray-5">{price}</p>
+        </div>
       </div>
-      <div className="flex items-center justify-between">
+      <div className="flex my-2 px-[8px] md:px-[16px] items-center justify-between text-[10px] md:text-[16px]">
+        <div className="flex flex-row-reverse items-center gap-1">
+          <p className="text-gray-4 text-[10px]">({fodes} امتیاز)</p>
+          {star}
+          <img src={starImageSrc[star - 1]} alt="" />
+        </div>
         <p>
           {discountedAmount}
           تومان
         </p>
-        <div className="flex items-center gap-1">
-          <p className="text-gray-4">({fodes} امتیاز)</p>
-          {star}
-          <img src={starImageSrc[star - 1]} alt="" />
-        </div>
       </div>
-      <button className="w-full py-2 text-white text-center text-[10px] md:text-[16px]">
-        افزودن به سبد خرید
-      </button>
+      <div className="px-[8px] md:px-[16px]">
+        <button className="w-full py-2 rounded-4 font-[400] text-white bg-primary  text-center text-[10px] md:text-[16px]">
+          افزودن به سبد خرید
+        </button>
+      </div>
     </div>
   );
 };
 
-export default boxFood;
+export default BoxFood;
---
+++ components/Slider-food.tsx
@@ -0,0 +1,46 @@
+import React, { useRef, useState } from "react";
+import BoxFood from "./Box-food";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+// import required modules
+import { Pagination } from "swiper/modules";
+
+export default function SliderBox({ data }: any) {
+  console.log(data);
+
+  return (
+    <div dir="rtl">
+      <Swiper
+        breakpoints={{
+          768: {
+            slidesPerView: 2,
+          },
+          1024: {
+            slidesPerView: 3
+          },
+          1280: {
+            slidesPerView: 4
+          }
+        }}
+        slidesPerView={1.6}
+        spaceBetween={20}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        {data.map((item: any) => (
+          <SwiperSlide>
+            <BoxFood {...item} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+}

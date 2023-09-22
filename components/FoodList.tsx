e64d0e68773a56d225b9a527a268c0b283423b87
RezaSiahi <rezasiahi8@gmail.com>
2023-09-23 00:38:34 +0330

    add Sandwich list in menu page

---
+++ components/FoodList.tsx
@@ -0,0 +1,17 @@
+import BoxFoodMenu from "./Box-food-menu"
+
+const FoodList = ({title, list}: any) => {
+    return ( 
+        <div className="px-[20px] my-3 md:my-4 md:px-[60px] lg:px-[90px]">
+          <p className="text-[16px] my-2 pt-4 pb-2 md:text-[20px] lg:text-[24px] font-[700]">
+              {title}
+          </p>
+          <div className="grid grid-cols-1 grid-rows-1 md:grid-rows-2 md:grid-cols-2 gap-3">
+            {list.map((item: any) => (
+              <BoxFoodMenu key={item.id} {...item} />
+            ))}
+          </div>
+        </div>
+    )
+}
+export default FoodList
--- pages/menu.tsx
+++ pages/menu.tsx
@@ -5,6 +5,7 @@ import SearchBox from "@/components/SearchBox";
 import { IoIosArrowBack } from "react-icons/io";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import BoxFoodMenu from "@/components/Box-food-menu";
+import FoodList from "@/components/FoodList";
 
 const menu = () => {
   const [filterFoodList, setFilterFoodList] = useState("غذای اصلی");
@@ -143,7 +144,8 @@ const menu = () => {
       price: 170000,
       discount: 17,
       star: 4,
-      compounds: "اسپاگتی، گوشت چرخ کرده، هویج، ساقه کرفس، گوجه فرنگی، سیر، پیاز، پنیر پارمزان، روغن زیتون",
+      compounds:
+        "اسپاگتی، گوشت چرخ کرده، هویج، ساقه کرفس، گوجه فرنگی، سیر، پیاز، پنیر پارمزان، روغن زیتون",
     },
     {
       id: 3,
@@ -152,7 +154,8 @@ const menu = () => {
       price: 180000,
       discount: 35,
       star: 4,
-      compounds: "بادمجان، گوجه فرنگی، کدو سبز، پیاز، رب گوجه فرنگی، روغن زیتون، پنیر پارمزان",
+      compounds:
+        "بادمجان، گوجه فرنگی، کدو سبز، پیاز، رب گوجه فرنگی، روغن زیتون، پنیر پارمزان",
     },
     {
       id: 4,
@@ -160,7 +163,8 @@ const menu = () => {
       name: "لازانیا",
       price: 150000,
       star: 5,
-      compounds: "لازانیا، قارچ، ریحان تازه، جعفری تازه، گوجه فرنگی، پنیر پیتزا، بادمجان",
+      compounds:
+        "لازانیا، قارچ، ریحان تازه، جعفری تازه، گوجه فرنگی، پنیر پیتزا، بادمجان",
     },
     {
       id: 5,
@@ -169,7 +173,8 @@ const menu = () => {
       price: 100000,
       discount: 15,
       star: 4,
-      compounds: "جلبک دریایی/ نوری، برنج کته، سرکه سفید (یا سرکه برنج)، شکر، نمک دریا",
+      compounds:
+        "جلبک دریایی/ نوری، برنج کته، سرکه سفید (یا سرکه برنج)، شکر، نمک دریا",
     },
     {
       id: 6,
@@ -187,7 +192,8 @@ const menu = () => {
       price: 19000,
       discount: 17,
       star: 5,
-      compounds: "اسفناج، قارچ، پنیر موزارلا یا پنیر پیتزا، پنیر ریکوتا یا پنیر خامه ای، پیاز، سیر، روغن زیتون",
+      compounds:
+        "اسفناج، قارچ، پنیر موزارلا یا پنیر پیتزا، پنیر ریکوتا یا پنیر خامه ای، پیاز، سیر، روغن زیتون",
     },
     {
       id: 8,
@@ -198,6 +204,118 @@ const menu = () => {
       star: 4,
       compounds: "پنیر، اسفناج، گوجه، پیاز، سیر ",
     },
+  ];
+
+  const pizaas = [
+    {
+      id: 1,
+      img: "/Images/pizzas/rokola.png",
+      name: "پیتزا روکولا",
+      price: 195000,
+      discount: 12,
+      star: 5,
+      compounds: "اسفناج، سبزی روکولا، آرد، پودر مایه خمیر، روغن زیتون، خردل، سیر، پنیر موزارلا و پارمسان، گوجه گیلاسی ،سس فلفل سبز تند ",
+    },
+    {
+      id: 2,
+      img: "/Images/pizzas/bademganzeyton.png",
+      name: "پیتزا بادمجان و زیتون",
+      price: 150000,
+      star: 4,
+      compounds: "بادمجان کوچک، روغن زیتون، پنیر موزارلا، پنیر پارمزان، برگ ریحان، گوجه فرنگی، سس گوجه فرنگی",
+    },
+    {
+      id: 3,
+      img: "/Images/pizzas/sabzigatVkhame.png",
+      name: "پیتزا سبزیجات و خامه",
+      price: 210000,
+      discount: 21,
+      star: 4,
+      compounds: "نخود فرنگی پخته شده، ذرت نیم پز، فلفل دلمه‌ای رنگی، قارچ، سیر یا پیازچه خردشده",
+    },
+    {
+      id: 4,
+      img: "/Images/pizzas/peyroni.png",
+      name: "پیتزا پیرونی",
+      price: 210000,
+      discount: 21,
+      star: 4,
+      compounds: "نخود فرنگی پخته شده، ذرت نیم پز، فلفل دلمه‌ای رنگی، قارچ، سیر یا پیازچه خردشده",
+    },
+    {
+      id: 5,
+      img: "/Images/pizzas/garch.png",
+      name: "پیتزا قارچ",
+      price: 210000,
+      discount: 15,
+      star: 3,
+      compounds: "قارچ، فلفل دلمه ای، رب گوجه فرنگی، پودر سیر، آویشن، مرزه، پنیر پیتزا گیاهی",
+    },
+    {
+      id: 6,
+      img: "/Images/pizzas/esfenag.png",
+      name: "پیتزا اسفناح",
+      price: 280000,
+      discount: 10,
+      star: 5,
+      compounds: "اسفناج تازه، پیاز، سیر، پنیر پیتزا، قارچ",
+    },
+    {
+      id: 7,
+      img: "/Images/pizzas/margarita.png",
+      name: "پیتزا مارگایتا",
+      price: 165000,
+      discount: 13,
+      star: 2,
+      compounds: "گوجه فرنگی، ریحان، سیر، پنیر پیتزا",
+    },
+    {
+      id: 8,
+      img: "/Images/pizzas/panir.png",
+      name: "پنیر پیتزا",
+      price: 125000,
+      discount: 16,
+      star: 3,
+      compounds: "نان پیتزا، پنیر پیتزا، سس باربیکیو، گوجه فرنگی، سس کچاپ، سیر، روغن زیتون",
+    },
+  ];
+
+  const sandevech = [
+    {
+      id: 1,
+      img: "/Images/sandevech/1.png",
+      name: "ساندویچ کتلت مخصوص",
+      price: 230000,
+      discount: 18,
+      star: 5,
+      compounds: "سیب زمینی، لوبیا قرمز،  بلغور گندم، نعناع خشک، پودر زیره، پودر جوز هندی، گوجه فرنگی، روغن زیتون",
+    },
+    {
+      id: 2,
+      img: "/Images/sandevech/2.png",
+      name: "ساندویچ سوسیس کوکتل",
+      price: 205000,
+      discount: 35,
+      star: 4,
+      compounds: "سوسیس گیاهی، پیاز، سیب زمینی،  رب گوجه فرنگی",
+    },
+    {
+      id: 3,
+      img: "/Images/sandevech/3.png",
+      name: "ساندویچ کتلت کدو سبز",
+      price: 145000,
+      star: 5,
+      compounds: "کدو سبز، هویج، سیب زمینی، پیاز",
+    },
+    {
+      id: 4,
+      img: "/Images/sandevech/4.png",
+      name: "پینی اسفناج",
+      price: 210000,
+      discount: 15,
+      star: 3,
+      compounds: " قارچ، اسفناج، پیاز، پنیر پیتزا، خامه، پودر پاپریکا، گردو، سرکه بالزامیک، سس مایونز",
+    },
   ]
 
   return (
@@ -206,7 +324,8 @@ const menu = () => {
         <SliderLanding />
         <div className="px-[20px] md:px-[60px] lg:px-[90px] py-2 flex items-center gap-3 bg-gray-3">
           {foodListName.map((item) => (
-            <p key={crypto.randomUUID()}
+            <p
+              key={crypto.randomUUID()}
               className={`${
                 filterFoodList === item &&
                 "text-primary border-b font border-primary"
@@ -220,7 +339,10 @@ const menu = () => {
         <div className="mb-3 mx-[20px] md:px-[60px] mt-2 xl:px-[90px] flex items-start md:items-center justify-between flex-col md:flex-row">
           <div className="flex items-center gap-1 md:gap-4 flex-wrap mt-2">
             {typesFood.map((item) => (
-              <div key={crypto.randomUUID()} className="flex items-center bg-gray-3 rounded-8 p-1">
+              <div
+                key={crypto.randomUUID()}
+                className="flex items-center bg-gray-3 rounded-8 p-1"
+              >
                 <p className="w-max text-[10px] md:text-[16px]">{item}</p>
                 <IoIosArrowBack />
               </div>
@@ -230,33 +352,18 @@ const menu = () => {
             <SearchBox />
           </div>
         </div>
-        <div className="flex justify-end -mb-12 md:-mb-12 mx-[20px] md:ml-[60px] lg:ml-[90px]">
+        <div className="flex justify-end -mb-16 md:-mb-12 mx-[20px] md:ml-[60px] lg:ml-[90px]">
           <div className="flex items-center my-3 p-1 md:p-2 gap-1 text-[12px] border border-primary rounded-4 text-primary px-1 w-fit cursor-pointer">
             <AiOutlineShoppingCart className="w-[16px] h-[16px]" />
             تکمیل خرید
           </div>
         </div>
-        <div className="px-[20px] md:px-[60px] lg:px-[90px]">
-          <p className="text-[16px] my-2 md:text-[20px] lg:text-[24px] font-[700]">
-            غذا های ایرانی
-          </p>
-          <div className="grid grid-cols-1 grid-rows-1 md:grid-rows-2 md:grid-cols-2 gap-3">
-            {iranianFood.map((item) => (
-              <BoxFoodMenu key={item.id} {...item} />
-            ))}
-          </div>
-        </div>
 
-        <div className="px-[20px] md:px-[60px] lg:px-[90px]">
-          <p className="text-[16px] my-2 md:text-[20px] lg:text-[24px] font-[700]">
-            غذا های غیر ایرانی
-          </p>
-          <div className="grid grid-cols-1 grid-rows-1 md:grid-rows-2 md:grid-cols-2 gap-3">
-            {nonIranianFood.map((item) => (
-              <BoxFoodMenu key={item.id} {...item} />
-            ))}
-          </div>
-        </div>
+
+        <FoodList title="غذا های ایرانی" list={iranianFood} />
+        <FoodList title="غذا های غیر ایرانی" list={nonIranianFood} />
+        <FoodList title="پیتزا ها" list={pizaas} />
+        <FoodList title="ساندویچ ها" list={sandevech} />
       </Layout>
     </>
   );

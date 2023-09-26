c6b4b8b1d102495406a3ccaae1f337dfd4475462
RezaSiahi <rezasiahi8@gmail.com>
2023-09-26 20:35:43 +0330

    add format price function

---
+++ utils/helper.ts
@@ -0,0 +1,8 @@
+const formatPrice = (price: string) => {
+  const cost = price;
+  return parseFloat(cost)
+    .toFixed(0)
+    .replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+};
+
+export {formatPrice}

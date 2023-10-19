318a1b00c639cf7367051b579af7841d1c3ad255
RezaSiahi <rezasiahi8@gmail.com>
2023-10-19 21:33:56 +0330

    add custom hook jwt_decode

--- hooks/decode_jwt.js
+++ hooks/decode_jwt.js
@@ -1,5 +1,6 @@
 import jwt_decode from 'jwt-decode';
 
-export const decodeJWT = ({token}) => {
-    return jwt_decode(token);
+export const decodeJWT = (token) => {
+    const {email, role} = jwt_decode(token)
+    return {email, role}
 }
